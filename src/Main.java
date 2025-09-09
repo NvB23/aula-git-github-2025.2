1e92f6c079bfc1fb6f061f22f8eeef06d0ab16ec
Naum <naumvb23@gmail.com>
2025-09-08 21:34:02 -0300

    Meu primeiro objeto
    Eu criei minha primeira classe e objeto

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class Main {
     public static int somar (int num1, int num2){
         return num1 + num2;
@@ -5,7 +7,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Olá, GitHub!");
 
-        int result = somar(5,6);
-        System.out.println(result);
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "Naum";
+        pessoa1.idade = 18;
+        System.out.println(pessoa1.nome);
+        System.out.println(pessoa1.idade);
     }
 }
